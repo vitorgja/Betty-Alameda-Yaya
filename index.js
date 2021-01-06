b0565d64cb44be99122a7e986b97844ff49bff8d
Vitor Pereira <vsantos@lemontech.com.br>
2021-01-06 12:41:35 -0300

    feat: Add command messages

--- index.js
+++ index.js
@@ -21,15 +21,23 @@ client.on("message", function (message) {
 
     if (command === "start") {
       activeChannel.push(message.channel.name);
-      message.reply(`Iniciando ${config.botname} em ${message.channel.name}`);
+      message.reply(`Iniciando ${config.botname} no canal ${message.channel.name}`);
     } else if (command === "stop") {
       activeChannel = activeChannel.filter(channel => channel != message.channel.name);
-      message.reply(`Parando ${config.botname} em ${message.channel.name}`);
+      message.reply(`Parando ${config.botname} no canal ${message.channel.name}`);
+    } else if (command === "messages") {
+      message.reply(
+        `Mensagens Listadas no ${config.botname}: \n` + 
+        msgs.messages
+          .map((message, index) => `${index + 1}. ${message}`)
+          .join("\n")
+      );
     } else if (command === "help") {
       var helpCommands = ['\n'];
       helpCommands.push(`Comandos ${config.botname} - ${config.prefix}help`);
       helpCommands.push(`${config.prefix}start - Inicia o ${config.botname}`);
       helpCommands.push(`${config.prefix}stop - Finaliza o ${config.botname}`);
+      helpCommands.push(`${config.prefix}messages - Lista as Mensagens da ${config.botname}`);
       helpCommands.push(`\nAcorde o bot em https://bot-betty-alameda-yaya.herokuapp.com/ `);
       message.reply(helpCommands.join('\n'));
     }
@@ -58,6 +66,10 @@ var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Listening at http://%s:%s', host, port);
+  console.log(
+    'Listening at http://%s:%s', 
+    host == '::' ? 'localhost' : host, 
+    port
+  );
 
 });
