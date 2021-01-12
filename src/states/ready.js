2db0b66b434f05eda0ff2635fc864b4caa76d435
Vitor Pereira <vsantos@lemontech.com.br>
2021-01-12 10:04:03 -0300

    fix: On bot wake up send message of the day

--- src/states/ready.js
+++ src/states/ready.js
@@ -5,7 +5,7 @@ const request = require('request'),
 const getMsgSemana = (function(date) {
   switch (date.getDay()) {
     case 1: return 'Segundou';
-    case 2: return 'Terçou';
+    case 2: return 'Tercou';
     case 3: return 'Quartou';
     case 4: return 'Quintou';
     case 5: return 'Sextou';
@@ -18,45 +18,43 @@ module.exports = {
   ready: function (client) {
     client.user.setActivity('https://bot-betty-alameda-yaya.herokuapp.com/', { type: 'WATCHING' });
 
-    console.log("Tst")
-
     const channelName = 'geral';
-    const channel = client.channels.cache.find(channel => channel.name === channelName)
-
-    // const channelID = '468838715377057802' // geral
-    // const channel = client.channels.cache.get(channelID)    
-
-    if (channel) {
-      const message = `Betty & Serjão`
-      channel.send(message)
+    const channels = client.channels.cache.filter(channel => channel.name === channelName);
+    
+    if (channels) {
+      const message = `Acordei Parças`;
+      channels.map( channel => channel.send(message) );
     }
 
     this.messagOfTheDay(client)
   },
 
   messagOfTheDay: function(client) {
-
-    // const URL_GIPHY = 'https://api.giphy.com/v1/gifs/trending';
+    /**
+     * API Url to Trending https://api.giphy.com/v1/gifs/trending
+     * API Url to Search https://api.giphy.com/v1/gifs/search
+     */
     const URL_GIPHY = 'https://api.giphy.com/v1/gifs/search';
     const api_key = 'Gc7131jiJuvI7IdN0HZ1D7nh0ow5BU6g';
-    const limit = 1;
-    const q = 'day'
+    const limit = 10;
     request
-    .get(`${URL_GIPHY}?api_key=${api_key}&limit=${limit}&q=${getMsgSemana}`,function(err, response, body) {
+    .get(`${URL_GIPHY}?api_key=${api_key}&limit=${limit}&q=${getMsgSemana}`, function(err, response, body) {
       body = JSON.parse(body);
       
       if (body && body.data && body.data.length > 0) {
+
+        // TODO: Get Random DATA
         const giff = body.data[0];
 
         const channelName = 'geral';
-        const channel = client.channels.cache.find(channel => channel.name === channelName)
+        const channels = client.channels.cache.filter(channel => channel.name === channelName)
 
-        if (channel) {
-          const message = `${giff.title} \n ${giff.images.original.url}`
-          channel.send(message)
+        if (channels) {
+          const message = `${giff.title} \n ${giff.images.original.url}`;
+          channels.map( channel => channel.send(message) );
         }
       } else {
-        console.log('Hoje não tem Giphy \n search: ${getMsgSemana}')
+        console.log(`Hoje não tem Giphy \n search: ${getMsgSemana}`);
       }
     })
     .on('error', function(err) {
